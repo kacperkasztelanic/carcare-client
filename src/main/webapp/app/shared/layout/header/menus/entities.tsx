703713e3f016177f513a75fb195e2260a9c4ef81
Kacper Kasztelanic <kcpr51@hotmail.com>
2018-11-10 20:06:42 +0100

    Fix undefined children warning concerning Entities menu

--- src/main/webapp/app/shared/layout/header/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/header/menus/entities.tsx
@@ -8,6 +8,7 @@ import { NavDropdown } from '../header-components';
 export const EntitiesMenu = props => (
   // tslint:disable-next-line:jsx-self-close
   <NavDropdown icon="th-list" name={translate('global.menu.entities.main')} id="entity-menu">
+  <div />
     {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
   </NavDropdown>
 );
