7309381f44bcdbcfd8674259534667d4722d7d00
Kacper Kasztelanic <kcpr51@hotmail.com>
2019-01-02 21:29:17 +0100

    Alter footer

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -8,7 +8,7 @@ const Footer = props => (
     <Row>
       <Col md="12">
         <p>
-          &copy; 2018 CarCare developed by Kacper Kasztelanic
+          &copy; 2018-2019 <img src="content/images/car.svg" alt="Logo" height="20" /> CarCare
         </p>
       </Col>
     </Row>
